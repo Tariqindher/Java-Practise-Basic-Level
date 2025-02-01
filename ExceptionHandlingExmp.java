f8252e42a8b12c39db4ef3ebbe6d02866f6e829e
Tariq Mehmood <tariqmehmoodsthp23@gmail.com>
2025-02-01 17:27:32 +0500

    Create ExceptionHandlingExmp.java
    Added my task file

---
+++ ExceptionHandlingExmp.java
@@ -0,0 +1,13 @@
+class ABC{
+public double Average(int Number) throws IllegalException{
+       new Ill
+}
+
+}
+public class ExceptionHandlingExmp{
+       public static void main(String args[]){
+
+              
+       }
+
+}
